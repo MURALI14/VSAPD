a2cebdc59a189182656231bc4d5eaac642069e3e
Murali14 <muralibharadhwaj@gmail.com>
2022-10-28 16:48:11 +0530

    EOFSEC4

--- APIDev/APIDev/Controllers/RegionsController.cs
+++ APIDev/APIDev/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using APIDev.Models.Domain;
+using APIDev.Models.DTO;
 using APIDev.Repositories;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -12,16 +13,16 @@ namespace APIDev.Controllers
         private readonly IRegionRepository regionRepository;
         private readonly IMapper mapper;
 
-        public RegionsController(IRegionRepository regionRepository,IMapper mapper)
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
         {
             this.regionRepository = regionRepository;
             this.mapper = mapper;
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();
-            
+
             //var regionsDTO = new List<Models.DTO.Region>();
 
             //return DTO regions
@@ -39,9 +40,99 @@ namespace APIDev.Controllers
             //};
             //regionsDTO.Add(regionDTO);
             //});
-            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions) ;
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
         }
-       
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+
+            return Ok(regionDTO);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(AddRegionRequest addRegionRequest)
+        {
+            //Request to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Name = addRegionRequest.Name,
+                Population = addRegionRequest.Population,
+
+            };
+            //pass details to repository
+            region = await regionRepository.AddAsync(region);
+            //convert back to DTO.
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id }, regionDTO);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionByIdAsync(Guid id)
+        {
+            //get reguion from database
+            var delRegion = await regionRepository.DeleteAsync(id);
+
+            //if null not found
+            if (delRegion == null)
+            {
+                return NotFound();
+            }
+            //convert to DTO
+            var deldto = mapper.Map<Models.DTO.Region>(delRegion);
+            //return ok
+            return Ok(deldto);
+
+        }
+        [HttpPut]
+        [Route("{id:guid}") ]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id,[FromBody]Models.DTO.AddRegionRequest upRegionRequest)
+        {
+            //dto to domain
+            var region = new Models.Domain.Region()
+            {
+                Code = upRegionRequest.Code,
+                Area = upRegionRequest.Area,
+                Lat = upRegionRequest.Lat,
+                Long = upRegionRequest.Long,
+                Name = upRegionRequest.Name,
+                Population = upRegionRequest.Population,
+
+            };
+
+            //update
+            region = await regionRepository.UpdateAsync(id, region);
+
+            //not null
+            if(region == null)
+            {
+                return NotFound();
+            }
+            //back to dto
+            var dtreg = mapper.Map<Models.DTO.Region>(region);
+            //return
+            return Ok(dtreg);
+        }
     }
 }
--- APIDev/APIDev/Repositories/RegionRepository.cs
+++ APIDev/APIDev/Repositories/RegionRepository.cs
@@ -11,6 +11,53 @@ namespace APIDev.Repositories
         {
             this.aPIDbContext = aPIDbContext;
         }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await aPIDbContext.AddAsync(region);
+            await aPIDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await aPIDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if(region == null)
+            {
+                return null; 
+            }
+            
+            aPIDbContext.Regions.Remove(region);
+            await aPIDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await aPIDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var curregion =  await aPIDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(curregion == null)
+            {
+                return null;
+            }
+            curregion.Code = region.Code;
+            curregion.Name = region.Name;
+            curregion.Area = region.Area;
+            curregion.Lat = region.Lat;
+            curregion.Long = region.Long;
+            curregion.Population = region.Population;
+
+            await aPIDbContext.SaveChangesAsync();
+
+            return curregion;
+        }
+
         async Task<IEnumerable<Region>> IRegionRepository.GetAllAsync()
         {
             return await aPIDbContext.Regions.ToListAsync();
