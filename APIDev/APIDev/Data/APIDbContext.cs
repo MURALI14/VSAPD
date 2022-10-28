c35a8921b3fde0220856243fcf295ebd9313cbc0
Murali14 <muralibharadhwaj@gmail.com>
2022-10-28 12:12:55 +0530

    EOFSEc2

---
+++ APIDev/APIDev/Data/APIDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using APIDev.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIDev.Data
+{
+    public class APIDbContext: DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walk { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ APIDev/APIDev/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace APIDev.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+
+        public string code { get; set; }
+    }
+}
